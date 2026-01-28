b4c27619c0805aaf3b9130c1b6aba70e6386619b
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2026-01-28 08:44:31 -0400

    First Commit

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgiaDavid_Interfaces
+{
+    class Player
+    {
+        Position _playerPosition;
+
+        public Player()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace GeorgiaDavid_Interfaces
 {
     internal class Program
     {
+        struct Position
+        {
+            public Position(int x, int y)
+            {
+                
+            }
+            
+        }
+        
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
